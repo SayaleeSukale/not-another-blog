c9f216a6dd240667dd5a80f8e6fb16d8311c2c82
SayaleeSukale <125121866+SayaleeSukale@users.noreply.github.com>
2025-10-27 20:44:51 +0530

    Initial backend setup

---
+++ src/main/java/com/project/notanotherblog/FullstackBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.notanotherblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBlogBackendApplication.class, args);
+	}
+
+}
