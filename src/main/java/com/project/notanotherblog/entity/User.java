b1803167e1ba61a9d673696e1fce74a0ce2d5ca3
SayaleeSukale <125121866+SayaleeSukale@users.noreply.github.com>
2025-10-29 11:56:31 +0530

    added service layer

--- src/main/java/com/project/notanotherblog/entity/User.java
+++ src/main/java/com/project/notanotherblog/entity/User.java
@@ -12,12 +12,16 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
 	@Id
